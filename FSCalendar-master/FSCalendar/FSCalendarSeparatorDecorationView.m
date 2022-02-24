c83ab7fe995909f59425bcf22e032dc6592d5f27
小文 <yaowenchen@caixin.com>
2022-02-24 17:18:48 +0800

    提交代码

---
+++ FSCalendar-master/FSCalendar/FSCalendarSeparatorDecorationView.m
@@ -0,0 +1,29 @@
+//
+//  FSCalendarSeparatorDecorationView.m
+//  FSCalendar
+//
+//  Created by 丁文超 on 2018/10/10.
+//  Copyright © 2018 wenchaoios. All rights reserved.
+//
+
+#import "FSCalendarSeparatorDecorationView.h"
+#import "FSCalendarConstants.h"
+
+@implementation FSCalendarSeparatorDecorationView
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        self.backgroundColor = FSCalendarStandardSeparatorColor;
+        self.backgroundColor = UIColor.redColor;
+    }
+    return self;
+}
+
+- (void)applyLayoutAttributes:(UICollectionViewLayoutAttributes *)layoutAttributes
+{
+    self.frame = layoutAttributes.frame;
+}
+
+@end
